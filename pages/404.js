ca1758b1a5f63865a536e96b2482fcd87b514358
IrfanAshraf-pro <irfanashraf4090@gmail.com>
2023-04-18 00:33:34 +0500

    Done till adding blog index page

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+import Layout from "@/components/Layout";
+import Image from "next/image";
+
+
+export default function NotFoundPage() {
+  return (
+    <Layout title='Page Not Found'>
+        <div className="flex flex-col items-center mt-20">
+            <Image src='/images/logo.png' width={70} height={70} className="bg-gray-800 rounded-2xl"/>
+            <h1 className="text-6xl my-5">Whooops!</h1>
+            <h2 className="text-4xl text-gray-400 mb-5">This Page Doesnot Exist</h2>
+        </div>
+    </Layout>
+  )
+}
